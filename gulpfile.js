b39f5b0e81d0445948ef5ad754ae901a9259523c
littleguga <fed777os@gmail.com>
2016-08-12 06:14:44 +0300

    update package.json and gulp.js to last web-template

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,64 @@
 /* Vars */
 var gulp = require('gulp'),
-    livereload = require('gulp-livereload'),
     cssmin = require('gulp-cssmin'),
-    rename = require('gulp-rename'),
     jsmin = require('gulp-jsmin'),
     concat = require('gulp-concat'),
     autoprefixer = require('gulp-autoprefixer'),
+    plumber = require('gulp-plumber'),
+
+    stylus = require('gulp-stylus'),
+    jade = require('gulp-jade'),
 
 //optional
     imagemin = require('gulp-imagemin'),
     pngquant = require('imagemin-pngquant'),
 
-    stylus = require('gulp-stylus'),
-    //preprocess = require('gulp-preprocess'),
-    jade = require('gulp-jade');
+    browserSync = require('browser-sync').create();
 
 
 /* Sources */
-var src_js = 'sources/js/**/*.js',
-    src_css = 'sources/css/**/*.css',
-    src_stylus = 'sources/css/**/*.styl',
-    src_html = 'sources/html/**/*.html',
-    src_img = 'sources/img/**/*',
-    src_ico = 'sources/img/favicon/*.ico',
-    src_jade = 'sources/html/**/*.jade',
-    src_fonts = 'sources/fonts/**/*';
+var src_path = 'sources/';
+var src_js = src_path + 'js/**/*.js',
+    src_css = src_path + 'css/**/*.css',
+    src_stylus = src_path + 'css/**/*.styl',
+    src_img = src_path + 'img/**/*',
+    src_ico = src_path + 'img/favicon/*.ico',
+    src_jade = src_path + 'html/**/*.jade',
+    src_fonts = src_path + 'fonts/**/*';
+
+var npm_src = [
+    //material-design-lite
+    {
+        src: 'node_modules/material-design-lite/material.min.*',
+        dest: 'material'
+    },
+
+    //flexboxgrid
+    {
+        src: 'node_modules/flexboxgrid/dist/flexboxgrid.min.*',
+        dest: 'css'
+    },
+
+    //normalize
+    {
+        src: 'node_modules/normalize.css/normalize.css',
+        dest: 'css',
+        minify: {
+            css: true
+        }
+    },
+
+    //jquery
+    {
+        src: 'node_modules/jquery/dist/jquery.min.js',
+        dest: 'js'
+    }
+];
 
 
 /* Destination folder */
-var DEST = 'build/';
-var dest_html = ""; // DEST + '';
+var DEST = 'dist/';
+var dest_html = DEST + '';
 
 
 /* Other */
@@ -41,43 +70,67 @@ var browsers_ver = ['not ie <= 9', 'iOS > 7'];
 /* Tasks */
 gulp.task('default', ['build', 'watch']);
 
-gulp.task('build', ['buildJs',
+gulp.task('build', [
+    'buildJs',
     'buildCss',
     'buildStylus',
-    'buildHtml',
     'buildJade',
     'buildFonts',
     'buildImg',
     'buildFavicon',
-    'buildDep'
+    'buildDeps'
 ]);
 
 
 // Watch Files For Changes
 gulp.task('watch', function () {
-    livereload.listen(); //default web-server
+    browserSync.init({
+        server: {
+            baseDir: "./dist/"
+        }
+    });
 
+    //watch sources
+    gulp.watch(src_jade, ['reloadJade']);
     gulp.watch(src_js, ['reloadJs']);
     gulp.watch(src_css, ['reloadCss']);
     gulp.watch(src_stylus, ['reloadStylus']);
-    gulp.watch(src_html, ['reloadHtml']);
-    gulp.watch(src_jade, ['reloadJade']);
     gulp.watch(src_img, ['reloadImg']);
     gulp.watch(src_fonts, ['reloadFonts']);
+
+    //Reload builded
+    gulp.watch(dest_html + '*.html').on('change', browserSync.reload);
+    gulp.watch(DEST + '/js/*').on('change', browserSync.reload);
+    gulp.watch(DEST + '/css/*').on('change', browserSync.reload);
+    gulp.watch(DEST + '/img/*').on('change', browserSync.reload);
+    gulp.watch(DEST + '/fonts/*').on('change', browserSync.reload);
 });
 
 /* -------------------- Dependencies */
-//Material
-gulp.task('buildDep', function () {
-    //material
-    gulp.src('node_modules/material-design-lite/*.min.css')
-        .pipe(gulp.dest(DEST + 'material'))
-        .pipe(livereload());
-    gulp.src('node_modules/material-design-lite/*.min.js')
-        .pipe(gulp.dest(DEST + 'material'))
-        .pipe(livereload());
-});
 
+gulp.task('buildDeps', function () {
+    npm_src.forEach(function (dep) {
+        //if we need to minify anything
+        if (dep.minify) {
+            //if we need to minify css
+            if (dep.minify.css) {
+                gulp.src(dep.src)
+                    .pipe(cssmin())
+                    .pipe(gulp.dest(DEST + dep.dest))
+            }
+
+            //if we need to minify js
+            if (dep.minify.js) {
+                gulp.src(dep.src)
+                    .pipe(jsmin())
+                    .pipe(gulp.dest(DEST + dep.dest))
+            }
+        } else {
+            gulp.src(dep.src)
+                .pipe(gulp.dest(DEST + dep.dest))
+        }
+    });
+});
 
 
 /* -------------------- JS */
@@ -86,17 +139,14 @@ gulp.task('reloadJs', function () {
     gulp.src(src_js)
         .pipe(concat("js.min.js"))
         .pipe(gulp.dest(DEST + 'js'))
-        .pipe(livereload());
 });
 
 //Build
 gulp.task('buildJs', function () {
     gulp.src(src_js)
         .pipe(jsmin())
-        .pipe(rename({suffix: '.min'}))
         .pipe(concat("js.min.js"))
         .pipe(gulp.dest(DEST + 'js'))
-        .pipe(livereload());
 });
 
 
@@ -106,7 +156,6 @@ gulp.task('reloadCss', function () {
     gulp.src(src_css)
         .pipe(concat("css.min.css"))
         .pipe(gulp.dest(DEST + 'css'))
-        .pipe(livereload());
 });
 
 //Build
@@ -119,17 +168,27 @@ gulp.task('buildCss', function () {
         .pipe(cssmin())
         .pipe(concat("css.min.css"))
         .pipe(gulp.dest(DEST + 'css'))
-        .pipe(livereload());
 });
 
 
 /* -------------------- Stylus */
 //Reload
-gulp.task('reloadStylus', ['buildStylus']);
+gulp.task('reloadStylus', function () {
+    gulp.src(src_stylus)
+        .pipe(plumber())
+        .pipe(stylus())
+        .pipe(autoprefixer({
+            browsers: browsers_ver,
+            cascade: false
+        }))
+        .pipe(concat("style.min.css"))
+        .pipe(gulp.dest(DEST + 'css'))
+});
 
 //Build
 gulp.task('buildStylus', function () {
     gulp.src(src_stylus)
+        .pipe(plumber())
         .pipe(stylus())
         .pipe(autoprefixer({
             browsers: browsers_ver,
@@ -138,43 +197,29 @@ gulp.task('buildStylus', function () {
         .pipe(cssmin())
         .pipe(concat("style.min.css"))
         .pipe(gulp.dest(DEST + 'css'))
-        .pipe(livereload());
 });
-
-
-/* -------------------- Html */
-//Reload
-gulp.task('reloadHtml', function () {
-    gulp.src(src_html)
-        .pipe(gulp.dest(dest_html))
-        .pipe(livereload());
-});
-
-//Build
-gulp.task('buildHtml', ['reloadHtml']);
-//see jade
 
 
 /* -------------------- Jade */
 //Reload
 gulp.task('reloadJade', function () {
     gulp.src(src_jade)
+        .pipe(plumber())
         .pipe(jade({
             locals: YOUR_LOCALS,
             pretty: true
         }))
         .pipe(gulp.dest(dest_html))
-        .pipe(livereload());
 });
 
 //Build
 gulp.task('buildJade', function () {
     gulp.src(src_jade)
+        .pipe(plumber())
         .pipe(jade({
             locals: YOUR_LOCALS
         }))
         .pipe(gulp.dest(dest_html))
-        .pipe(livereload());
 });
 
 
@@ -191,14 +236,12 @@ gulp.task('buildImg', function () {
             use: [pngquant()]
         }))
         .pipe(gulp.dest(DEST + 'img'))
-        .pipe(livereload());
 });
 
 //Build
 gulp.task('buildFavicon', function () {
     gulp.src(src_ico)
         .pipe(gulp.dest(dest_html))
-        .pipe(livereload());
 });
 
 
@@ -210,16 +253,4 @@ gulp.task('reloadFonts', ['buildFonts']);
 gulp.task('buildFonts', function () {
     gulp.src(src_fonts)
         .pipe(gulp.dest(DEST + 'fonts'))
-        .pipe(livereload());
 });
-
-
-/* -------------------- Other */
-//Gulp-preprocess example
-/*
- gulp.task('test-preprocess', function () {
- gulp.src('test.css')
- .pipe(preprocess({context: {RELEASE_TAG: 'here goes our replace'}}))
- .pipe(rename({suffix: '.b'}))
- .pipe(gulp.dest(''));
- });*/
